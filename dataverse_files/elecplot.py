9e2c8caa6efd9d2864bd415ea5a48f7245f0beae
Yash Jain <62240937+yashjain0@users.noreply.github.com>
2021-04-28 11:02:24 +0530

    election dataset plot by matplotlib and pandas

---
+++ dataverse_files/elecplot.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+df = pd.read_stata(dtafile)
+dtafile = 'Bhavnani India national election dataset v 2.0.dta'
+df = pd.read_stata(dtafile)
+#this code is for plotting contents of our election database.
